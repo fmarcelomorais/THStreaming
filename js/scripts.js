31caf95ad361f4887f6ce523cc9de077a259b2c8
fmarcelomorais <marcelomorais.program@gmail.com>
2023-04-25 07:59:49 -0300

    inclui pagina de revendedor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 import QRCode from "./qrcode.js";
 
-
 const IDPAYLOADFORMATINDICATOR = '00';
 const IDMERCHANTACCOUNTINFORMATION = '26';
 const IDMERCHANTACCOUNTINFORMATIONGUI = '00';
@@ -20,12 +19,13 @@ let codigoGerado = "";
 
 const recebedor = "FRANCISCO MARCELO SERRA MORAIS"
 const chavePix = '5869dc96-a3af-4f86-837f-b8d56b86fca3'
-const nome = document.getElementById('nome');
-const telefone = document.getElementById('telefone')
-const valor = document.getElementById('plano');
+const nome = document.getElementById('nomeRevendedor');
+const telefone = document.getElementById('contatoRevendedor');
+const email = document.getElementById('emailRevendedor');
+const valor = document.getElementById('planoRevendedor');
 const descricao = "DESCRICAO";
 const txId = "THSTREAMING"
-const botao = document.getElementById('botaoGeraPix')
+const botao = document.getElementById('btnRev')
 
 
 function getValue(id, valor){
@@ -80,7 +80,7 @@ function getPayload(){
     payload += getValue(IDTRANSACTIONAMOUNT,  valor.value )
     payload += getValue(IDCOUNTRYCODE, 'BR')
         + getValue(IDMERCHANTNAME, recebedor.substring(0,25))
-        + getValue(IDMERCHANTCITY, 'FORTALEZA' /*cidade*/ ) //testei com Fortaleza mas da erro no código
+        + getValue(IDMERCHANTCITY, 'FORTALEZA' /*cidade*/ )
         + getAddionalDataFielTemplate();
         
     codigoGerado = getCRC16(payload).toString()
@@ -89,7 +89,7 @@ function getPayload(){
 }
 
 
-const btn = document.querySelector('#btn')
+const btn = document.querySelector('#btnModal')
 const codigo = document.querySelector('.codigo')
 const qrcodeCopy = document.querySelector('.qrcodeCopy')
 const titulo = document.querySelector('.modal-title')
@@ -97,40 +97,58 @@ const titulo = document.querySelector('.modal-title')
 function geradorChavePix(e){
     let plano = "";
 
-    if(valor.value == 29.90){
-        plano = "PLANO MENSAL"
+    if(valor.value == 100){
+        plano = "PLANO EMPREENDEDOR OURO"
     }
-    if(valor.value == 79.90){
-        plano = "PLANO TRIMESTRAL"
+    if(valor.value == 150){
+        plano = "PLANO EMPREENDEDOR PRATA"
     }
-    if(valor.value == 169.90){
-        plano = "PLANO SEMESTRAL"
+    if(valor.value == 25){
+        plano = "PLANO EMPREENDEDOR OURO"
     }
-    if(valor.value == 299.90){
-        plano = "PLANO ANUAL"
-    }
-    if(valor.value == 49.90){
-        plano = "PLANO MENSAL + INTERNET"
-    }
-
+    
+    swal({
+        title: "IMPORTANTE!",
+        text: "O Telefone de contato deve estar correto pois ele será usado para entrar-mos em contato como você para cadastrar seus dados de recebimento.",
+        icon: "warning",
+    });
     
     e.preventDefault()
-    if(nome.value && telefone.value && valor.value){
+
+    
+    if(nome.value && telefone.value && valor.value && email.value){
         btn.removeAttribute("disabled")   
-        titulo.innerHTML = `${nome.value}, Esse é seu código, use a câmera para escanear ou copie e cole o código.` 
-        let texto = `<h4>Você escolheu o <b>${plano}</b>, no valor de <b>R$ ${valor.value}</b>.\nMande o comprovante para um dos whatsapp (85) 98216-1439 ou  (85) 98795-9500.</h4>`
+        titulo.innerHTML = `${nome.value}, Esse é seu QRCode, use a câmera para escanear ou copie e cole o código.` 
+        let texto = `<h4>Você escolheu o <b>${plano}</b>, no valor de <b>R$ ${valor.value},00 </b>.\nMande o comprovante para um dos whatsapp (85) 98216-1439 ou  (85) 98795-9500.</h4>`
         texto += `<p>Código Copia e cola:<p> <h5>${getPayload()}</h5>`
         
         codigo.innerHTML = texto
         
         new QRCode(qrcodeCopy, getPayload());
-            
+        
+        sendEmail(email.value, texto) 
+
         }else{
             swal('Preencha todos os campos para criar o seu QRCODE.')
         }
 
      
 }
+
+function sendEmail(email, texto) {
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "th.streamingtv@gmail.com",
+      Password: "Dinha1409.",
+      To: email,
+      From: "th.streamingtv@gmail.com",
+      Subject: "CADASTRO DE REVENDEDOR",
+      Body: texto,
+    })
+      .then(function (message) {
+        swal("mail sent successfully")
+      });
+  }
 
 botao.addEventListener('click', geradorChavePix)
 
