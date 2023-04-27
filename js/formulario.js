131d619593b5910dfe262f3bb95378e2af43d250
fmarcelomorais <marcelomorais.program@gmail.com>
2023-04-26 23:58:31 -0300

    altera modalidade de revenda

--- js/formulario.js
+++ js/formulario.js
@@ -31,27 +31,55 @@ function geradorChavePixRevendedor(e){
 function enviarEmail(e){
     const chavePix = geradorChavePixRevendedor(e)
     let plano = "";
-
-    if(planoRevendedor.value == 100){
-        plano = "PLANO EMPREENDEDOR OURO"
-    }
-    if(planoRevendedor.value == 150){
-        plano = "PLANO EMPREENDEDOR PRATA"
-    }
-    if(planoRevendedor.value == 25){
-        plano = "PLANO EMPREENDEDOR BRONZE"
-    }
-
     let texto = 'EXCELENTE DECISÃO!\n'
-    texto += 'Falta pouco para você se tornar um de nossos revendedores de sucesso.\n'
-    texto += `Recebedor: FRANCISCO MARCELO SERRA MORAIS - BANCO: EFI\n`
-    texto += `Cópia e cola: ${chavePix}\n\n`
-    texto += `Chave Pix: th.streamingtv@gmail.com -> valor R$${planoRevendedor.value},00\n\n`
-    texto += `Enviar comprovante para um dos WhatsApp: 85 98216-1439 | 85 98795-9500 `
-    texto += `ou Enviar comprovante para th.streamingtv@gmail.com.\n\n`
-    texto += `O link do painel com login e senha será enviado após confirmação do pagamento.\n`
-    texto += `Enviar numero de whatsapp para ser incluido no grupo de suporte.\n`
-    texto += `Boas Vendas.\n`
+
+    if(planoRevendedor.value == 60){
+        plano = "PLANO EMPREENDEDOR INICIANTE"
+    }
+    if( planoRevendedor.value == 100){
+        plano = "PLANO EMPREENDEDOR MODERADO"
+    }
+    if( planoRevendedor.value == 190){
+        plano = "PLANO EMPREENDEDOR EXPERT"
+    }
+    if( planoRevendedor.value == 250 ){
+        plano = "PLANO EMPREENDEDOR SUPER"
+    }
+    if( planoRevendedor.value == 400 ){
+        plano = "PLANO EMPREENDEDOR MASTER"
+    }
+    if( planoRevendedor.value == 700 ){
+        plano = "PLANO EMPREENDEDOR MÁXIMO"
+    }
+    if( planoRevendedor.value == 25 ){
+        plano = "PLANO EMPREENDEDOR - CONFIANÇA"
+    }
+
+    if(planoRevendedor.value > 25){
+        texto = 'EXCELENTE DECISÃO!\n'
+        texto += 'Falta pouco para você se tornar um de nossos revendedores de sucesso.\n'
+        texto += `Recebedor: FRANCISCO MARCELO SERRA MORAIS - BANCO: EFI\n`
+        texto += `Cópia e cola: ${chavePix}\n\n`
+        texto += `Chave Pix: th.streamingtv@gmail.com -> valor R$${planoRevendedor.value},00\n\n`
+        texto += `Enviar comprovante para um dos WhatsApp: 85 98216-1439 | 85 98795-9500 `
+        texto += `ou Enviar comprovante para th.streamingtv@gmail.com.\n\n`
+        texto += `O link do painel com login e senha será enviado após confirmação do pagamento.\n`
+        texto += `Enviar numero de whatsapp para ser incluido no grupo de suporte.\n`
+        texto += `Boas Vendas.\n`
+    }else{
+        texto = 'EXCELENTE DECISÃO!\n'
+        texto += 'Falta pouco para você se tornar um de nossos revendedores de sucesso.\n'
+        texto += `Recebedor: FRANCISCO MARCELO SERRA MORAIS - BANCO: EFI\n`
+        texto += `Cópia e cola: ${chavePix}\n\n`
+        texto += `Chave Pix: th.streamingtv@gmail.com -> valor R$${planoRevendedor.value},00\n\n`
+        texto += `Enviar comprovante para um dos WhatsApp: 85 98216-1439 | 85 98795-9500 `
+        texto += `ou Enviar comprovante para th.streamingtv@gmail.com.\n\n`
+        texto += `O link do painel com login e senha será enviado após confirmação do pagamento.\n`
+        texto += `Enviar numero de whatsapp para ser incluido no grupo de suporte.\n`
+        texto += `Boas Vendas.\n\n`
+        texto += `Ativação do cliente feita pela TH Streaming (Após confirmação de pagamento)\n`
+        texto += `Nessa modalidade a venda é divida em 50% para o revendedor e a TH STREAMING.\n`
+    }
 
     fetch("https://formsubmit.co/ajax/th.streamingtv@gmail.com", {
         method: "POST",
